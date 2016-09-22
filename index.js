db7ca8e51171b6a5095099285ec59954b73442ff
jaspercurry <jaspercurry@gmail.com>
2016-09-22 09:29:43 -0400

    made map lab

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+
+
+var sortedRobots = robots.map(function (robot) {
+  if (knownDecepticons.indexOf(robot.name) >= 0) {
+    return Object.assign({}, robot, { alliance: "decepticon" })
+  } else { return Object.assign({}, robot, { alliance: "autobot" })
+    }
+});
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +30,11 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null },
 ];
+
+var coloredZebraStripes = zebraStripes.map(function(zebra, index) {
+  if (index % 2 == 0) {
+    return Object.assign({}, zebra, { color: "black" })
+  } else {
+    return Object.assign({}, zebra, { color: "white" })
+  }
+});
